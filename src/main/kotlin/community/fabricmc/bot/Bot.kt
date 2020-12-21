b37882c9715cc5b2a6ec7f9bf9d2c7da28277fad
Gareth Coles <gdude2002@gmail.com>
2020-12-21 17:51:34 +0000

    Linting and making it run

--- src/main/kotlin/community/fabricmc/bot/Bot.kt
+++ src/main/kotlin/community/fabricmc/bot/Bot.kt
@@ -4,28 +4,33 @@ import com.kotlindiscord.kord.extensions.ExtensibleBot
 import com.kotlindiscord.kord.extensions.utils.respond
 import com.kotlindiscord.kordex.ext.mappings.MappingsExtension
 
-private const val modDevAlt = 789958205211803698L
+@Suppress("UnderscoresInNumericLiterals")  // It's a Snowflake, really
+private const val MOD_DEV_ALT = 789958205211803698L
 
+/** Global bot object. **/
 val bot = ExtensibleBot(
     System.getenv("BOT_TOKEN"),
     prefix = "?",
     addSentryExtension = false
 )
 
+/** Launch function. **/
 suspend fun main() {
     MappingsExtension.addCheck { command ->
         { event ->
             var result = true
 
-            if (!command.startsWith("y")) {
-                if (event.message.channelId.value != modDevAlt) {
-                    event.message.respond("Non-Yarn commands may not be used outside of <#$modDevAlt>.")
+            if (!command.startsWith("y") && event.message.channelId.value != MOD_DEV_ALT) {
+                event.message.respond("Non-Yarn commands may not be used outside of <#$MOD_DEV_ALT>.")
 
-                    result = false
-                }
+                result = false
             }
 
             result
         }
     }
+
+    bot.addExtension(MappingsExtension::class)
+
+    bot.start()
 }
